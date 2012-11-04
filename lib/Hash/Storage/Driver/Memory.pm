9973ab1c08f2c023f88653d61f69c57c343a853e
Viktor Turskyi <viktor@webbylab.com>
2012-11-04 09:13:43 +0200

    Added "Memory" driver

---
+++ lib/Hash/Storage/Driver/Memory.pm
@@ -0,0 +1,51 @@
+package Hash::Storage::Driver::Memory;
+
+use v5.10;
+use strict;
+use warnings;
+use File::Slurp;
+use Hash::Storage::Util qw/do_exclusively/;
+use List::Util qw/max/;
+use base "Hash::Storage::Driver::Base";
+
+sub init {
+    my ($self) = @_;
+    $self->{data} = {};
+}
+
+sub get {
+    my ( $self, $id ) = @_;
+    my $hashes = $self->{data};
+
+    return $hashes->{$id};
+}
+
+sub set {
+    my ( $self, $id, $fields ) = @_;
+    my $hashes = $self->{data};
+    
+    @{ $hashes->{$id} }{ keys %$fields } = values %$fields;
+}
+
+sub del {
+    my ( $self, $id ) = @_;
+    my $hashes = $self->{data};
+
+    delete $hashes->{$id};
+}
+
+sub list {
+    my ( $self, @query ) = @_;
+    my $hashes = $self->{data};
+
+    my @hashes = values %$hashes;
+    return $self->do_filtering(\@hashes, \@query);
+}
+
+sub count {
+    my ( $self, @query ) = @_;
+    my $hashes = $self->list(@query);
+    return scalar(@$hashes);
+}
+
+1;    # End of Hash::Storage
